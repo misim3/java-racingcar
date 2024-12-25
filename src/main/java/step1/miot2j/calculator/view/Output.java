077915f3ca02a57cf5cd924fd6efc60136544cf6
misim3 <sim000314@naver.com>
2024-12-25 23:19:07 +0900

    feat: 계산기 output 클래스 구현

---
+++ src/main/java/step1/miot2j/calculator/view/Output.java
@@ -0,0 +1,16 @@
+package step1.miot2j.calculator.view;
+
+public class Output {
+
+    private static final String START_MESSAGE = "계산식을 입력하세요.";
+    private static final String CALCULATE_RESULT = "계산 결과: ";
+
+    public static void outputStartMessage() {
+        System.out.println(START_MESSAGE);
+    }
+
+    public static void resultOutput(double result) {
+        System.out.print(CALCULATE_RESULT);
+        System.out.println(result);
+    }
+}
