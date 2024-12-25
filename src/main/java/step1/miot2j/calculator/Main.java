ec69ecfb81f11f7e0668426e0da62adca445488b
misim3 <sim000314@naver.com>
2024-12-25 23:18:29 +0900

    feat: 계산기 main 클래스 구현

---
+++ src/main/java/step1/miot2j/calculator/Main.java
@@ -0,0 +1,10 @@
+package step1.miot2j.calculator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.printResult();
+    }
+
+}
