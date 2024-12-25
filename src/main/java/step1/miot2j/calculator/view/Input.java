255bcb07076879377a10e4dd37114dde8d0f2565
misim3 <sim000314@naver.com>
2024-12-26 06:54:32 +0900

    feat: 계산기 Input 클래스 구현

---
+++ src/main/java/step1/miot2j/calculator/view/Input.java
@@ -0,0 +1,12 @@
+package step1.miot2j.calculator.view;
+
+import java.util.Scanner;
+
+public class Input {
+
+    public static String inputMathExpression() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+}
