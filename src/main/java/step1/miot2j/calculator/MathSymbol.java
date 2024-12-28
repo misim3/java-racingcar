401c983ce0c78037fce685c6f2fddfebb814265c
misim3 <sim000314@naver.com>
2024-12-28 14:44:21 +0900

    feat: MathSymbol 클래스 구현

---
+++ src/main/java/step1/miot2j/calculator/MathSymbol.java
@@ -0,0 +1,45 @@
+package step1.miot2j.calculator;
+
+import java.util.Arrays;
+
+import static step1.miot2j.calculator.ExceptionMessage.IS_NOT_MATH_EXPRESSION_MESSAGE;
+
+public enum MathSymbol {
+    PLUS("+"),
+    MINUS("-"),
+    DIVIDE("/"),
+    MULTIPLE("*");
+
+    private final String mathSymbol;
+
+    MathSymbol(String mathSymbol) {
+        this.mathSymbol = mathSymbol;
+    }
+
+    public String getMathSymbol() {
+        return mathSymbol;
+    }
+
+    public static MathSymbol findValidatedSymbol(String operator) {
+        return Arrays.stream(MathSymbol.values())
+            .filter(v -> v.getMathSymbol().equals(operator))
+            .findAny()
+            .orElseThrow(() -> new IllegalArgumentException(String.format("잘못 된 연산자입니다.")));
+    }
+
+    public double operate(double firstNumber, double secondNumber) {
+        if (PLUS.getMathSymbol() == this.mathSymbol) {
+            return firstNumber + secondNumber;
+        }
+        if (MINUS.getMathSymbol() == this.mathSymbol) {
+            return firstNumber - secondNumber;
+        }
+        if (DIVIDE.getMathSymbol() == this.mathSymbol) {
+            return firstNumber / secondNumber;
+        }
+        if (MULTIPLE.getMathSymbol() == this.mathSymbol) {
+            return firstNumber * secondNumber;
+        }
+        throw new IllegalArgumentException(IS_NOT_MATH_EXPRESSION_MESSAGE);
+    }
+}
