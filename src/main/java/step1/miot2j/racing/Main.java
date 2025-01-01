c124328a037fecc57a09a0fa7093cb16fd6d61a7
misim3 <sim000314@naver.com>
2025-01-01 22:33:41 +0900

    feat: RacingCar Main 클래스 구현

---
+++ src/main/java/step1/miot2j/racing/Main.java
@@ -0,0 +1,10 @@
+package step1.miot2j.racing;
+
+public class Main {
+
+    public static void main(String[] args) {
+        RacingGame racingGame = new RacingGame();
+        racingGame.gameStart();
+    }
+
+}
