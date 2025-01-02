6bdfcf0fe06462d929b112260c96b228f0d9a906
misim3 <sim000314@naver.com>
2025-01-02 21:14:56 +0900

    refactor: RacingGame 클래스 수정

--- src/main/java/step1/miot2j/racing/RacingGame.java
+++ src/main/java/step1/miot2j/racing/RacingGame.java
@@ -1,5 +1,17 @@
 package step1.miot2j.racing;
 
+import static step1.miot2j.racing.utils.RandomNumber.makeOneRandomNumber;
+import static step1.miot2j.racing.view.Input.makeCarNames;
+import static step1.miot2j.racing.view.Input.makeGameRepeatCount;
+import static step1.miot2j.racing.view.Output.printInputCountMessage;
+import static step1.miot2j.racing.view.Output.printNowDistance;
+import static step1.miot2j.racing.view.Output.printResultMessage;
+import static step1.miot2j.racing.view.Output.printStartMessage;
+import static step1.miot2j.racing.view.Output.printWinMessage;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RacingGame {
 
     public static String SEPARATOR = ",";
@@ -9,6 +21,7 @@ public class RacingGame {
         printStartMessage();
         String carNames = makeCarNames();
         printInputCountMessage();
+        int count = makeGameRepeatCount();
         printResultMessage();
 
         String[] carNameArray = carNames.split(SEPARATOR);
@@ -16,7 +29,77 @@ public class RacingGame {
 
         repeatMoveCars(count, carList);
         printWinMessage(findWinner(carList));
-        
+
     }
 
+    private List<Car> makeCarList(String[] carNameArray) {
+
+        List<Car> carList = new ArrayList<>();
+
+        for (String carName : carNameArray) {
+
+            carList.add(new Car(carName));
+        }
+
+        return carList;
+
+    }
+
+    private void repeatMoveCars(int count, List<Car> carList) {
+
+        for (int i = 0; i < count; i++) {
+
+            moveCars(carList);
+
+        }
+
+    }
+
+    private void moveCars(List<Car> carList) {
+
+        for (int i = 0; i < carList.size(); i++) {
+
+            carList.get(i).move(makeOneRandomNumber());
+
+        }
+
+        printNowDistance(carList);
+    }
+
+    private List<String> findWinner(List<Car> carList) {
+
+        List<String> winnerList = new ArrayList<>();
+        int winnerCondition = findWinnerCondition(carList);
+
+        for (Car car : carList) {
+
+            if (car.getMoveCount() == winnerCondition) {
+
+                winnerList.add(car.getName());
+
+            }
+
+        }
+
+        return winnerList;
+
+    }
+
+    private int findWinnerCondition(List<Car> carList) {
+
+        int winnerCondition = 0;
+
+        for (Car car : carList) {
+
+            if (car.getMoveCount() >= winnerCondition) {
+
+                winnerCondition = car.getMoveCount();
+
+            }
+
+        }
+
+        return winnerCondition;
+
+    }
 }
